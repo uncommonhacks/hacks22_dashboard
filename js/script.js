d125ee8b32bdfd2c9d8cfedd8e74bd4e4821321c
Jim Liu <liuz@uchicago.edu>
2022-03-29 17:13:40 -0400

    unstyled data in

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 
+function time_format(hour, minute){
+    var am_pm;
+    if (hour >= 12) {
+        am_pm = 'pm';
+        hour = hour-12;
+    }
+    else {
+        am_pm = 'am';
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    var time_string = hour + ':' + minute + ' ' + am_pm;
+    return time_string
+}
 function update(){
     console.log("lul");
     var current = new Date();
-    var cur_hours = current.getUTCHours()-5; //delete +1 later   
+    var cur_hours = current.getUTCHours()-5;
     var cur_minutes = current.getUTCMinutes();
     var cur_day = 1;//current.getUTCDate();
     var days = DATA["days"];
     var cur_event = -1;
     var next_event = -1;
     for (const day of days){
-        console.log(day);
         if (day['day'] == cur_day) {
             var events = day['events']
             for (const event of events) {
@@ -30,12 +44,26 @@ function update(){
     }
     if (cur_event != -1) {
         var div1 = document.getElementById('cur_event');
-        div1.innerHTML = cur_event['text']
+        div1.innerHTML = cur_event['text'];
+        var div2 = document.getElementById('cur_event_time');
+        var hour = cur_event['hour'];
+
+        var minute = cur_event['minute'];
+        var time_string = time_format(hour, minute);
+        div2.innerHTML = time_string;
     }
     if (next_event != -1) {
-        var div2 = document.getElementById('next_event');
-        div2.innerHTML = next_event['text']
+        var div1 = document.getElementById('next_event');
+        div1.innerHTML = next_event['text'];
+        var div2 = document.getElementById('next_event_time');
+        var hour = next_event['hour'];
+
+        var minute = next_event['minute'];
+        var time_string = time_format(hour, minute);
+        div2.innerHTML = time_string;
     }
+    var div = document.getElementById('current_time');
+    div.innerHTML = time_format(cur_hours, cur_minutes);
     
 }
 
