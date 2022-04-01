d4f6ccdd62673720405f211670cf5a8cf3fe1126
Spencer Ng <sng1488@gmail.com>
2022-04-01 10:46:37 -0500

    Embed rockwell

--- js/schedule.js
+++ js/schedule.js
@@ -1,30 +1,30 @@
 var DATA = {
     "days":[
         {"day":1, "events":[
-            {"hour":17,"minute":0, "location":"JCL 298; GatherT 1", "text":"CSS still life"},
-            {"hour":18,"minute":30, "location":"JCL 298; GatherT 1<br>JCL 298; GatherT 1", "text":"dinner<br>intro to twine games"},
-            {"hour":19,"minute":30, "location":"JCL 298; GatherT 1", "text":"social time with UH team"}
+            {"hour":17,"minute":0, "location":"JCL 298", "text":"CSS still life"},
+            {"hour":18,"minute":30, "location":"JCL 298<br>JCL 298", "text":"dinner<br>intro to twine games"},
+            {"hour":19,"minute":30, "location":"JCL 298", "text":"social time with UH team"}
         ]},
         {"day":2, "events":[
-            {"hour":11,"minute":0, "location":"JCL 298; GatherT 1", "text":"check-in + sponsor fair"},
-            {"hour":12,"minute":0, "location":"JCL 298; GatherT 1", "text":"team formation"},
-            {"hour":1,"minute":0, "location":"JCL 298; GatherT 1", "text":"opening ceremony"},
-            {"hour":2,"minute":0, "location":"JCL 298; GatherT 1", "text":"hacking starts"},
-            {"hour":3,"minute":0, "location":"JCL 298; GatherT 1", "text":"how social factors influence video games"},
-            {"hour":12,"minute":0, "location":"JCL 298; GatherT 1", "text":"board game design"},
-            {"hour":5,"minute":0, "location":"JCL 298; GatherT 1", "text":"intro to computational art"},
-            {"hour":6,"minute":0, "location":"JCL 298; GatherT 1", "text":"maritime archeology workshop"},
-            {"hour":6,"minute":30, "location":"JCL 298; GatherT 1", "text":"dinner"},
-            {"hour":7,"minute":0, "location":"JCL 298; GatherT 1", "text":"MLH event: slideshow karaoke"},
-            {"hour":8,"minute":0, "location":"JCL 298; GatherT 1", "text":"mystery event"}
+            {"hour":11,"minute":0, "location":"JCL 298", "text":"check-in + sponsor fair"},
+            {"hour":12,"minute":0, "location":"JCL 298", "text":"team formation"},
+            {"hour":1,"minute":0, "location":"JCL 298", "text":"opening ceremony"},
+            {"hour":2,"minute":0, "location":"JCL 298", "text":"hacking starts"},
+            {"hour":3,"minute":0, "location":"JCL 298", "text":"how social factors influence video games"},
+            {"hour":12,"minute":0, "location":"JCL 298", "text":"board game design"},
+            {"hour":5,"minute":0, "location":"JCL 298", "text":"intro to computational art"},
+            {"hour":6,"minute":0, "location":"JCL 298", "text":"maritime archeology workshop"},
+            {"hour":6,"minute":30, "location":"JCL 298", "text":"dinner"},
+            {"hour":7,"minute":0, "location":"JCL 298", "text":"MLH event: slideshow karaoke"},
+            {"hour":8,"minute":0, "location":"JCL 298", "text":"mystery event"}
         ]},
         {"day":3, "events":[
-            {"hour":9,"minute":30, "location":"JCL 298; GatherT 1", "text":"morning meditation"},
-            {"hour":11,"minute":0, "location":"JCL 298; GatherT 1", "text":"pitching workshop"},
-            {"hour":11,"minute":30, "location":"JCL 298; GatherT 1", "text":"hacker peer feedback session"},
-            {"hour":14,"minute":0, "location":"JCL 298; GatherT 1", "text":"hacking ends"},
-            {"hour":14,"minute":0, "location":"JCL 298; GatherT 1", "text":"project expo + judging"},
-            {"hour":16,"minute":30, "location":"JCL 298; GatherT 1", "text":"closing ceremony + awards"}
+            {"hour":9,"minute":30, "location":"JCL 298", "text":"morning meditation"},
+            {"hour":11,"minute":0, "location":"JCL 298", "text":"pitching workshop"},
+            {"hour":11,"minute":30, "location":"JCL 298", "text":"hacker peer feedback session"},
+            {"hour":14,"minute":0, "location":"JCL 298", "text":"hacking ends"},
+            {"hour":14,"minute":0, "location":"JCL 298", "text":"project expo + judging"},
+            {"hour":16,"minute":30, "location":"JCL 298", "text":"closing ceremony + awards"}
         ]}
     ]
 }
